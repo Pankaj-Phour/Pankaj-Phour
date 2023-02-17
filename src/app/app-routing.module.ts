cb201ad25d366e3935825591f7b83f8ec2949825
Pankaj Phour <pankaj.phour70@gmail.com>
2023-02-17 18:03:52 +0530

    UI changed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { LandingComponent } from './landing/landing.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 const routes: Routes = [
-  {
-    path:'', component: HomeComponent
-  }
+  {path:'', component: LandingComponent},
+  {path:'nav', component: NavbarComponent},
+  {path:'homw', component: HomeComponent}
 ];
 
 @NgModule({
